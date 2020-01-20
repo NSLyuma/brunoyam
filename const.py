b8c8887aaba199d233c31cb7dfce87ca37dac9c1
NSLyuma <59055680+NSLyuma@users.noreply.github.com>
2020-01-20 22:04:20 +0300

    Add files via upload

---
+++ const.py
@@ -0,0 +1,14 @@
+from express import Expression, Pow
+
+
+class Const(Expression):
+
+    def __init__(self, value):
+        super().__init__()
+        self.value=value
+
+    def calc(self):
+        return self.value
+
+p=Pow(Const(2), Const(5))
+print(p.calc())
