f7788c6072ab0b5fe0a024f87a89a2bb02c5c226
NSLyuma <59055680+NSLyuma@users.noreply.github.com>
2020-01-27 22:08:15 +0300

    Add files via upload

---
+++ excercises.py
@@ -0,0 +1,7 @@
+# A+B
+inp=input().split()
+a=int(inp[0])
+b=int(inp[1])
+print(a+b)
+
+#
---
+++ pony_orm.py
@@ -0,0 +1,47 @@
+from pony.orm import *
+
+db = Database()
+db.bind(provider='sqlite', filename='db.sqlite', create_db=True)
+
+
+class Client(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    phone = Optional(str, default="000000")
+    orders = Set('Order')
+
+
+class Order(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    products = Required(str)
+    client_id = Required(Client)
+
+
+
+db.generate_mapping(create_tables=True)
+sql_debug(True)
+
+
+@db_session
+def add_client(name, phone='000000'):
+    Client(name=name, phone=phone)
+
+@db_session
+def show_client():
+    select((p.id, p.name) for p in Client).show()
+
+@db_session
+def update_client():
+    clients=select(p for p in Client if p.name=='Petr')
+    print(clients[:])
+    for cl in clients[:]:
+        cl.name='Ivan'
+
+@db_session
+def delete_client(name):
+    delete(p for p in Client if p.name==name)
+
+
+if __name__=='__main__':
+   delete_client('Oleg')
+   show_client()
